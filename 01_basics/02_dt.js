bda75af848b0756593fb8ae643e8ad3c48943c5b
ChahakHarinkhede <146674984+ChahakHarinkhede@users.noreply.github.com>
2023-10-22 11:18:14 +0000

    02_dt.js

---
+++ 01_basics/02_dt.js
@@ -0,0 +1,31 @@
+//datatypes
+
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3+3);
+
+console.log("ankit"); // code readability should be high
+
+// documantation tc39(ecma script) and mdn
+
+let name = "ankit"
+let age = 20
+let ioLoggedIn = false 
+
+// number =>  range 2 to power 53
+//bigstring
+//string = ""
+//boolean =  true/false
+//null = standalone value
+//undefined = 
+//symbol
+
+
+//object
+
+
+console.log(typeof"ankit");
+console.log(typeof null); // object
+console.log(typeof undefined); // unsefined
